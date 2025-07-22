0167f9f18157732b9900b0c7679ba8be8ab6baef
supastishn <omareeto2012@hotmail.com>
2025-07-22 10:35:48 +0000

    .

--- main.js
+++
@@ -1,8 +0,0 @@
-import { updateAuthUI } from './authService.js';
-document.addEventListener('DOMContentLoaded', async () => {
-  try {
-    await updateAuthUI();
-  } catch (e) {
-    console.error('Auth initialization failed:', e);
-  }
-});
